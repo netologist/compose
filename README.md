4cb51c039d932ffa9c04713d0b48520ffeb2f205
Hasan Ozgan <hozgan@ozgan.net>
2025-03-15 18:58:01 +0000

    add templates

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 docker compositions
 
 ```sh
-curl -s -L https://raw.githubusercontent.com/netologist/compose/main/install.sh | sh
+curl -s -L https://raw.githubusercontent.com/netologist/docker-compose-templates/main/install.sh | bash
 
 ```
--- install.sh
+++ install.sh
@@ -1,4 +1,48 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
-echo "Hello"
+# set -e
+
+if [ -f docker-compose.yaml ] || [ -f docker-compose.yml ]; then
+    echo "docker-compose.yaml already exists. Do you want to overwrite it? (y/n)"
+    read overwrite < /dev/tty
+    if [ "$overwrite" != "y" ]; then
+        echo "Aborted"
+        exit 1
+    fi
+fi
+
+compositions=()
+for fullpath in templates/*; do 
+    filename=$(basename -- "$fullpath"); 
+    compositions+=("${filename%.*}")
+done
+length=${#compositions[@]}
+
+echo ""
+echo "Available templates:"
+
+for ((i=1; i<=$length; i++)); do
+    echo "$i) ${compositions[$i-1]}"
+done
+
+echo ""
+echo "Enter the number of the template you want to install:"
+read number < /dev/tty
+
+# Check if input is a valid integer
+if ! [[ "$number" =~ ^[0-9]+$ ]]; then
+    echo "Error: Not a valid number"
+    exit 1
+fi
+
+# Now check range with valid integer
+if [ "$number" -lt 1 ] || [ "$number" -gt "$length" ]; then
+    echo "Invalid number: out of range"
+    exit 1
+fi
+
+compose=${compositions[$number-1]}
+echo "Copying $compose template..."
+
+rm -f docker-compose.yml || true
+curl -s https://raw.githubusercontent.com/netologist/docker-compose-templates/main/templates/$compose.yaml > docker-compose.yaml
